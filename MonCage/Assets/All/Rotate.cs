bad5cded7b280087970e48de2764033a9528a54b
Việt Nguyễn Thế <83101689+theviet17@users.noreply.github.com>
2023-11-23 17:45:13 +0700

    first

---
+++ MonCage/Assets/All/Rotate.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotateSpeed = 1f;
+
+    [HideInInspector]public float mouseX;
+
+    [HideInInspector]public float mouseY;
+    public float a;
+    public float b;
+
+  
+    private void Update()
+    {
+        if (Input.GetMouseButton(0)) 
+        {
+            mouseX = Input.GetAxis("Mouse X") * rotateSpeed;
+            mouseY = Input.GetAxis("Mouse Y") * rotateSpeed;
+            mouseX = mouseX*a;
+            mouseY = mouseY * b;
+            transform.RotateAround(Vector3.down,mouseX);
+            transform.RotateAround(Vector3.right,mouseY);
+        }
+       
+    }
+}
